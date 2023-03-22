47f22f41d63055556d0b1fc782aeaba859f44d7b
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-22 07:47:18 +0100

    wip

--- deploy.bat
+++ deploy.bat
@@ -1,42 +1,29 @@
 @echo off
 
-REM Rename composer.json to composer.tmp.json
-rename composer.json composer.tmp.json
+:: Step 1: Rename composer.json to composer.tmp.json
+ren composer.json composer.tmp.json
 
-REM Rename composer.remote.json to composer.json
-rename composer.remote.json composer.json
+:: Step 2: Rename composer.remote.json to composer.json
+ren composer.remote.json composer.json
 
-REM Commit changes with the message "wip"
+:: Step 3: Make a composer update
+composer update
+
+:: Step 4: Make a GIT commit with message "wip"
 git add .
 git commit -m "wip"
 
-REM Store the current branch name
-for /f "tokens=*" %%a in ('git rev-parse --abbrev-ref HEAD') do set CURRENT_BRANCH=%%a
+:: Step 5: Make a GIT Force Push on the branch "DEV"
+git push --force origin DEV
 
-REM Check if the "dev" branch is checked out
-if "%CURRENT_BRANCH%"=="dev" (
-    echo The "dev" branch is currently checked out. Switching to "master" or "main" branch temporarily.
-    git checkout master || git checkout main
-)
+:: Step 6: Rename back composer.json to composer.remote.json
+ren composer.json composer.remote.json
 
-REM Force push the changes to the "dev" branch in the remote repository
-git push --force origin HEAD:dev
+:: Step 7: Rename back composer.tmp.json to composer.json
+ren composer.tmp.json composer.json
 
-REM Fetch changes from the remote repository
-git fetch origin
+:: Step 8: Make again composer update
+composer update
 
-REM Reset the local "dev" branch to match the remote "dev" branch
-git branch -f dev origin/dev
-
-REM Checkout the previous branch
-git checkout %CURRENT_BRANCH%
-
-REM Reset the git state to undo the last commit
-git reset HEAD~1
-
-REM Revert the filenames to the original state
-rename composer.json composer.remote.json
-rename composer.tmp.json composer.json
-
-REM Clear the environment variable
-set CURRENT_BRANCH=
+echo All tasks completed successfully.
+pause
