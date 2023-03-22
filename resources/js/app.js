8389b0278c3a3226f5f3c780450a112c0d0e5d9a
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-22 08:58:23 +0100

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+import.meta.glob([
+  '../images/**'
+  ]);
+
+// https://animxyz.com/docs
+import '@animxyz/core'
+
+// https://jquery.com
+import jquery from 'jquery';
+window.$ = jquery;
